d26df0c107e757dc1349a0dea6491096f1110548
Markim Shaw <ms79723@gmail.com>
2021-05-19 20:06:10 -0500

    Start of something new

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import Express from 'express';
+import { json } from 'body-parser';
+
+const PORT = 4000;
+
+const app = Express();
+
+app.use(json());
+
+app.get('/', (request, response) => {
+  response.send('This works');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
