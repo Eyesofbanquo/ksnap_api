f9c4f289bbae26a8d6617c114b47870d9dab419d
Markim Shaw <ms79723@gmail.com>
2021-05-21 15:36:41 -0500

    Guessing

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,9 @@
 import { Client } from 'pg';
 
+console.log(process.env.DATABASE_URL);
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 client.connect();
 
